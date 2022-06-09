4593afbd1659c13dd693440575d964201608125e
Zacharia <ndunguzachary24@gmail.com>
2022-06-10 00:10:40 +0300

    commit tree 3e0b0977efc27604007117e83394ab97ed505df4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers =( ) =>{
+    return drivers.slice(0, 2)
+}
+const returnLastTwoDrivers = () => {
+    return drivers.slice(2)
+}
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+ const createFareMultiplier = (fare) =>{
+     return fareMultiplier (createFareMultiplier(fare))
+ }
+
+
+
+
+
+
+
